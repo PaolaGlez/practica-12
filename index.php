60d40e6efff805aa734d165727a10b45701334c5
PaolaGlez <30761163+PaolaGlez@users.noreply.github.com>
2017-10-11 20:59:08 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title> Pagina 12-variable PHP </title>
+</head>
+<body>
+<?php
+$nombre='Paola';
+$edad=20;
+$salario=8500.50;
+$estatus=true;
+echo "Valores<br/>";
+echo "NOMBRE:".$nombre.'<BR/>';
+echo "EDAD" .$edad.'<BR/>';
+echo "SALARIO".$salario.'<BR/>';
+echo "ESTATUS:".$estatus.'<BR/>';
+?>
+<h1>OPERACIONES ARITMETICAS</H1>
+<?PHP
+$valor1=15;
+$valor2=7;
+$suma=$valor1+$valor2;
+$resta=$valor1-$valor2;
+$divide=$valor1/$valor2;
+$multiplicacion=$valor1*$valor2;
+echo "El valor 1 es:".$valor1. '<BR/>';
+echo "El valor 2 es:".$valor2. '<BR/>';
+echo "La suma es:".$suma. '<BR/>';
+echo "La resta es:".$resta. '<BR/>';
+echo "La division es:".$divide. '<BR/>';
+echo "La multiplicacion es:".$multiplicacion;
+?>
+</body>
+</html>
